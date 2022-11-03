7c5d4e6584baa30499bdad471d611adb76c5d662
LabuPaim <victorpaim80@gmail.com>
2022-11-03 11:16:49 -0300

    nest auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CandidatoModule } from './candidatos/candidato.module';
 import { RedirectController } from './redirect/redirect.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER}.8uowe0t.mongodb.net/?retryWrites=true&w=majority`),
-    CandidatoModule
+    CandidatoModule,
+    AuthModule
   ],
   controllers: [RedirectController],
   providers: [],
